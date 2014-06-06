3a1290a23374bc52e7633fd1df1a1cee025546be
Sartaj Singh <singhsartaj94@gmail.com>
2014-06-07 02:55:58 +0530

    Basic naming

--- ChatCli.py
+++ ChatCli.py
@@ -6,7 +6,7 @@ import socket, sys, struct
 
 si = struct.Struct('!I')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = sys.argv.pop() if len(sys.argv) == 3 else '127.0.0.1'
+HOST = sys.argv.pop() if len(sys.argv) == 4 else '127.0.0.1'
 PORT = 1060
 
 def recv_all(sock, length):
@@ -26,7 +26,7 @@ def get(sock):
 def put(sock, message):
     sock.send(si.pack(len(message)) + message)
 
-if sys.argv[1:] == ['server']:
+if sys.argv[1:2] == ['server']:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
@@ -34,30 +34,36 @@ if sys.argv[1:] == ['server']:
     sc, sockname = s.accept()
     print 'We have accepted a connection from', sockname 
     print 'Socket connects', sc.getsockname(), 'and', sc.getpeername()
+    server = sys.argv.pop()
+    put(sc, server)
+    client = get(sc)
     while True:
         message = get(sc)
         if(message == 'leobye'):
             break
-        print 'The incoming message says', repr(message)
-        send = raw_input('>>> ')
+        print client, '>>>', repr(message)
+        send = raw_input(server + ' >>> ')
         put(sc, send)
         if(send == 'leobye'):
             break
     sc.close()
     s.close()
 
-elif sys.argv[1:] == ['client']:
+elif sys.argv[1:2] == ['client']:
     s.connect((HOST, PORT))
     print 'Client has been assigned socket name', s.getsockname()
+    client = sys.argv.pop()
+    server = get(s)
+    put(s, client)
     while True:
-        send = raw_input('>>> ')
+        send = raw_input(client + ' >>> ')
         put(s, send)
         if(send == 'leobye'):
             break
         reply = get(s)
         if(reply == 'leobye'):
             break
-        print 'The server said', repr(reply)
+        print server, '>>>', repr(reply)
     s.close()
 
 else:
